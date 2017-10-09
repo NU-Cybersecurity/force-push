1d8de27314936d4cc7f00ec41e8e2edb8fea4cab
John Galbraith <jagalbraith@icloud.com>
2017-10-08 21:46:06 -0500

    Create fp_mail.py and modules directory.

---
+++ modules/fp_email.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''
+Module written by: John Galbraith
+
+
+'''
+
+import traceback
+
+
+class MAIL(object):
+    def __init__(self):
+        pass
+
+
+    def format_email(self):
+        pass
+
+
+    def send_email(self):
+        pass
+
+
+def main():
+    mail = MAIL()
+    mail.format_email()
+    mail.send_email()
+
+
+if '__main__' in __name__:
+    try:
+        main()
+    except:
+        print(traceback.format_exc())
