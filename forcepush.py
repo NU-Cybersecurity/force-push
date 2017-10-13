29ae8303ea59709bcf0624f1dbe9eed10017ae7d
John Galbraith <jagalbraith@icloud.com>
2017-10-12 23:23:54 -0500

    Set up modules and arguments base structure.

--- forcepush.py
+++ forcepush.py
@@ -7,7 +7,7 @@ Written by: John Galbraith, name, name, name, name
 
 '''
 
-import argparse #Needed to parse arguments
+import modules.fp_args
 import traceback #Needed for troubleshooting
 
 
@@ -16,11 +16,41 @@ def main():
     This is the main file for forcepush where it all comes
     together.
     '''
-    print('Hello World')
+    # 1. Check account and make sure super user.
+    ###Local code
+    ### Ask for output email address if not specified in cmd line
+
+    # 2. Get the arguments.
+    argmod = modules.fp_args.PARSE()
+    arguments = argmod.args()
+    print(arguments)
+
+    # 3. Determine red or blue
+    ### Local Code # Update here
+
+    # 4. Determine output and set up report file
+    ### Local code if email, Call fp_output.py if stdout
+
+    ### Red
+    # Run NMAP module
+    ###Call fp_scan.py
+
+    ### Blue
+    # Run the monitor mode module
+    ### Call fp_monitor.py
+
+    # 5. If --attack
+    ### Call fp_attack.py
+
+    # 6. If email send email.
+    ### Call fp_output.py
 
 
 if '__main__' in __name__:
     try:
         main()
+    except SystemExit:
+        print('So.... Do you feel like a hacker now?\n')
     except:
+        ### Call fp_output.py and send error email.
         print(traceback.format_exc())
