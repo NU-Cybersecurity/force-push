eb140f95f3376fca816e6e5ce777ebc6859893a8
B8P3 <32530730+B8P3@users.noreply.github.com>
2017-10-28 12:44:27 -0700

    Update fp_osscan.py

--- modules/fp_osscan.py
+++ modules/fp_osscan.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 
+#Importing the modules we'll need
 import nmap
 import getpass
 
+#Defining our clas and function
 class OSDETECT(object):
     def __init__(self):
         pass
 
-
+#Using nmap to scan for the host OS
     def osscan(self, host):
-        if 'root' in getpass.getuser():
+        if 'root' in getpass.getuser():         #Must be looged in as a root user or use sudo
             n = nmap.PortScanner()
-            n.scan(host, arguments='-sT -O')
+            n.scan(host, arguments='-sT -O')    #Adding arguments to the scan to get the target OS
             if 'osclass' in n[host]:
                 for opt in n[host]['osclass']:
-                    return opt
-            else:
+                    return opt                 
+            else:                              #Incase nmap was unable to determine the OS
                 return 'Unable to determine OS.'
-        else:
+        else:                                  #Incase the user isn't root or using sudo
             return 'You must run as ROOT for the os scan option.'
 
 
 if '__main__' in __name__:
     odt = OSDETECT()
-    print(odt.osscan('127.0.0.1'))
+    print(odt.osscan('127.0.0.1'))              #Display the OS found by the scan
